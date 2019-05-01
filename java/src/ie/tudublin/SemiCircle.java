5937e6534b9a7c33ebc9c405953ebbf090be6d0e
IvanRivan <c17463744@mydit.ie>
2019-05-01 07:49:34 +0100

    added semicircle class to program

---
+++ java/src/ie/tudublin/SemiCircle.java
@@ -0,0 +1,39 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class SemiCircle
+{
+    private float radius;
+    private UI ui;
+    private int x;
+    private int y;
+    private int h;
+
+    public SemiCircle(UI ui, float radius, int x, int y, int h)
+    {
+        this.ui = ui;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.h = h;
+    }
+
+    public void render()
+    {
+        int outline = h - 40;
+        float expand = radius + 10;
+        float inter = radius;
+
+        if (ui.mouseX > x-radius && ui.mouseX < x+radius && ui.mouseY > y-radius && ui.mouseY < y+radius)
+        {
+            inter = PApplet.map(ui.mouseX, 0, 30, radius, expand);
+        }
+        ui.stroke(outline);
+        ui.strokeWeight(20);
+        ui.fill(h);
+        ui.circle(x, y, inter); 
+    }
+
+
+}
