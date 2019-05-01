c232b6ca68a4daa62b075bfae5ef739250ff7d7c
IvanRivan <c17463744@mydit.ie>
2019-05-01 06:21:42 +0100

    set private fields for centercircle

---
+++ java/src/ie/tudublin/CenterCircle.java
@@ -0,0 +1,15 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class CenterCircle
+{
+    private float radius;
+    private UI ui;
+    private int x;
+    private int y;
+    private int fillcol;
+
+    
+}
+
